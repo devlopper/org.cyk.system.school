df6902d211381945b32abe39f8f2e67b5ad6a600
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-03-18 12:03:29 +0000

    Change-Id: I2c0eb545bc1e15c0c60ddc5d234754611f0f742c

--- school-model/src/main/java/org/cyk/system/school/model/actor/Student.java
+++ school-model/src/main/java/org/cyk/system/school/model/actor/Student.java
@@ -8,8 +8,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 import org.cyk.system.root.model.party.person.AbstractActor;
+import org.cyk.utility.common.annotation.ModelBean;
+import org.cyk.utility.common.annotation.ModelBean.CrudStrategy;
+import org.cyk.utility.common.annotation.ModelBean.GenderType;
 
-@Getter @Setter @Entity
+@Getter @Setter @Entity @ModelBean(genderType=GenderType.MALE,crudStrategy=CrudStrategy.BUSINESS)
 public class Student extends AbstractActor implements Serializable {
 
 	private static final long serialVersionUID = 2742833783679362737L;
