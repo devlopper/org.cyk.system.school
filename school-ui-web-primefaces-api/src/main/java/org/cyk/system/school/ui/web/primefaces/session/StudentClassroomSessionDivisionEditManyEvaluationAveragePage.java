d919439d940c58e858bcd9c83a1990e09025f153
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2016-05-17 11:50:50 +0000

    Change-Id: I055c001a322b3f670312112d37f39969f297144e

--- school-ui-web-primefaces-api/src/main/java/org/cyk/system/school/ui/web/primefaces/session/StudentClassroomSessionDivisionEditManyEvaluationAveragePage.java
+++ school-ui-web-primefaces-api/src/main/java/org/cyk/system/school/ui/web/primefaces/session/StudentClassroomSessionDivisionEditManyEvaluationAveragePage.java
@@ -7,6 +7,10 @@ import javax.faces.model.SelectItem;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import org.cyk.system.school.business.impl.SchoolBusinessLayer;
 import org.cyk.system.school.model.session.StudentClassroomSessionDivision;
 import org.cyk.ui.api.model.AbstractItemCollection;
 import org.cyk.ui.api.model.AbstractItemCollectionItem;
@@ -15,64 +19,44 @@ import org.cyk.ui.web.primefaces.page.crud.AbstractEditManyPage;
 import org.cyk.utility.common.annotation.user.interfaces.Input;
 import org.cyk.utility.common.annotation.user.interfaces.InputNumber;
 
-import lombok.Getter;
-import lombok.Setter;
-
 @Named @ViewScoped @Getter @Setter
-public class StudentClassroomSessionDivisionEditManyEvaluationAveragePage extends AbstractEditManyPage<StudentClassroomSessionDivision,StudentClassroomSessionDivisionEditManyEvaluationAveragePage.FormModel> implements Serializable {
+public class StudentClassroomSessionDivisionEditManyEvaluationAveragePage extends AbstractEditManyPage<StudentClassroomSessionDivision,StudentClassroomSessionDivisionEditManyEvaluationAveragePage.Form> implements Serializable {
 
 	private static final long serialVersionUID = 3274187086682750183L;
 	
 	@Override
-	protected void initialisation() {
-		super.initialisation();
-		elementCollection.getItemCollectionListeners().add(new ItemCollectionWebAdapter<FormModel,StudentClassroomSessionDivision>(){
-			private static final long serialVersionUID = -3872058204105902514L;
+	protected Class<Form> getItemCollectionItemClass() {
+		return Form.class;
+	}
+	
+	@Override
+	protected void create() {
+		SchoolBusinessLayer.getInstance().getStudentClassroomSessionDivisionBusiness().update(elementCollection.getIdentifiables());
+	}
+	
+	@Override
+	public ItemCollectionWebAdapter<Form, StudentClassroomSessionDivision> getItemCollectionAdapter() {
+		return new ItemCollectionAdapter<StudentClassroomSessionDivision,Form>(businessEntityInfos){
+			private static final long serialVersionUID = -5381415970572336750L;
+			
 			@Override
-			public void instanciated(AbstractItemCollection<FormModel, StudentClassroomSessionDivision,SelectItem> itemCollection,FormModel item) {
+			public void instanciated(AbstractItemCollection<Form, StudentClassroomSessionDivision, SelectItem> itemCollection,Form item) {
 				super.instanciated(itemCollection, item);
 				item.setValue(item.getIdentifiable().getResults().getEvaluationSort().getAverage().getValue());
-				debug(item.getIdentifiable().getResults().getEvaluationSort());
-			}	
-		});
-	}
-	
-	@Override
-	protected Class<FormModel> getItemCollectionItemClass() {
-		return FormModel.class;
-	}
-	
-	@Override
-	public ItemCollectionWebAdapter<FormModel, StudentClassroomSessionDivision> getItemCollectionAdapter() {
-		return new CustomItemCollectionAdapter();
+			}
+			
+			@Override
+			public void write(Form item) {
+				super.write(item);
+				item.getIdentifiable().getResults().getEvaluationSort().getAverage().setValue(item.getValue());
+			}
+		};
 	}
 	
 	/**/
 	
-	public static class CustomItemCollectionAdapter extends ItemCollectionAdapter<StudentClassroomSessionDivision,FormModel>  implements Serializable {
-		private static final long serialVersionUID = 7806030819027062650L;
-		@Override
-		public void instanciated(AbstractItemCollection<FormModel, StudentClassroomSessionDivision, SelectItem> itemCollection,FormModel item) {
-			super.instanciated(itemCollection, item);
-			item.setValue(item.getIdentifiable().getResults().getEvaluationSort().getAverage().getValue());
-			
-			System.out.println(
-					"StudentClassroomSessionDivisionEditManyEvaluationAveragePage.CustomItemCollectionAdapter.instanciated()");
-			debug(item.getIdentifiable().getResults().getEvaluationSort());
-		}
-		
-		@Override
-		public void write(FormModel item) {
-			super.write(item);
-			item.getIdentifiable().getResults().getEvaluationSort().getAverage().setValue(item.getValue());
-			System.out.println(
-					"StudentClassroomSessionDivisionEditManyEvaluationAveragePage.CustomItemCollectionAdapter.write()");
-			debug(item);
-		}
-	}
-	
 	@Getter @Setter
-	public static class FormModel extends AbstractItemCollectionItem<StudentClassroomSessionDivision> implements Serializable{
+	public static class Form extends AbstractItemCollectionItem<StudentClassroomSessionDivision> implements Serializable{
 		
 		private static final long serialVersionUID = -829786138986362643L;
 
