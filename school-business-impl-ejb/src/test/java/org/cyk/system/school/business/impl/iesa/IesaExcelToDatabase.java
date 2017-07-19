f5cad40b73aa67a3a30404c602f54a664dbf4639
Christian Yao Komenan <christian.komenan@dgbf.gouv.ci>
2017-07-19 14:58:03 +0000

    Change-Id: If636a7e61c8f3882e86a11011f7b63ef90af2809

--- school-business-impl-ejb/src/test/java/org/cyk/system/school/business/impl/iesa/IesaExcelToDatabase.java
+++ school-business-impl-ejb/src/test/java/org/cyk/system/school/business/impl/iesa/IesaExcelToDatabase.java
@@ -1,11 +1,8 @@
 package org.cyk.system.school.business.impl.iesa;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 
-import org.cyk.system.root.business.api.party.person.JobTitleBusiness;
-import org.cyk.system.root.business.api.party.person.PersonBusiness;
 import org.cyk.system.root.business.impl.AbstractBusinessTestHelper.TestCase;
 import org.cyk.system.root.business.impl.AbstractFakedDataProducer;
 import org.cyk.system.root.business.impl.BusinessInterfaceLocator;
@@ -14,7 +11,6 @@ import org.cyk.system.root.model.AbstractIdentifiable;
 import org.cyk.system.root.model.globalidentification.GlobalIdentifier;
 import org.cyk.system.root.model.party.person.JobTitle;
 import org.cyk.system.root.model.party.person.Person;
-import org.cyk.system.root.persistence.api.party.person.PersonDao;
 import org.cyk.system.school.business.impl._dataproducer.IesaFakedDataProducer;
 import org.cyk.system.school.business.impl.report.InternationalEnglishSchoolOfAbidjanReportProducer;
 import org.cyk.system.school.model.SchoolConstant;
@@ -42,13 +38,11 @@ public class IesaExcelToDatabase extends AbstractIesaBusinessIT {
     	AbstractRootReportProducer.DEFAULT = new InternationalEnglishSchoolOfAbidjanReportProducer();    	
     	
     	loadGlobalIdentifiers();
-    	createJobTitle();
+    	createIdentifiable(JobTitle.class, Boolean.TRUE, FieldHelper.getInstance().buildPath(AbstractIdentifiable.FIELD_GLOBAL_IDENTIFIER,GlobalIdentifier.FIELD_IDENTIFIER));
     	
-    	createPersons();
-    	//createPersons();
-    	//createPersons();
-    	
-    	//persons();
+    	createIdentifiable(JobTitle.class, Boolean.TRUE, FieldHelper.getInstance().buildPath(AbstractIdentifiable.FIELD_GLOBAL_IDENTIFIER,GlobalIdentifier.FIELD_IDENTIFIER));
+    	createIdentifiable(Person.class, Boolean.TRUE, FieldHelper.getInstance().buildPath(AbstractIdentifiable.FIELD_GLOBAL_IDENTIFIER,GlobalIdentifier.FIELD_IDENTIFIER)
+    			,Person.FIELD_LASTNAMES);
     	
     	testCase.assertIdentifiable(JobTitle.class,"SUPERVISOR", "Nursery and Primary Supervisor");
     	testCase.assertIdentifiable(JobTitle.class,"HS DIRECTOR", "High School Director");
@@ -99,12 +93,22 @@ public class IesaExcelToDatabase extends AbstractIesaBusinessIT {
     	setGlobalIdentifier(Arrays.asList(identifiable));
     }
     
-    private <T extends AbstractIdentifiable> void createIdentifiable(Class<T> aClass,Boolean globalIdentifier,String...fields){
+    private <T extends AbstractIdentifiable> void createIdentifiable(Class<T> aClass,final Boolean globalIdentifier,String...fields){
     	InstanceHelper.Pool.getInstance().load(aClass);
     	MicrosoftExcelHelper.Workbook.Sheet.Builder builder = new MicrosoftExcelHelper.Workbook.Sheet.Builder.Adapter.Default(workbookFileName,aClass);    	
-    	builder.createMatrix().getMatrix().getRow().setFromIndex(1).createKeyBuilder(new Object[]{0}, InstanceHelper.getInstance()
-    			.callGetMethod(InstanceHelper.Pool.getInstance().get(aClass), String.class, GlobalIdentifier.FIELD_CODE));
-		MicrosoftExcelHelper.Workbook.Sheet sheet = builder.execute();
+    	builder.createMatrix().getMatrix().getRow().setFromIndex(1);
+		builder.getMatrix().getRow().setKeyBuilder(new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
+			private static final long serialVersionUID = 1L;
+    		@Override
+    		protected Key __execute__() {
+    			return new ArrayHelper.Dimension.Key(Boolean.TRUE.equals(globalIdentifier) ? getGlobalIdentifier((String)getInput()[0]).getCode() : (String)getInput()[0]);
+    		}
+    	});
+    	builder.getMatrix().getRow().getKeyBuilder().addParameters(new Object[]{0});
+    	builder.getMatrix().getRow().addIgnoredKeyValues(InstanceHelper.getInstance().callGetMethod(InstanceHelper.Pool.getInstance().get(aClass), String.class
+    			, GlobalIdentifier.FIELD_CODE));	
+    	
+    	MicrosoftExcelHelper.Workbook.Sheet sheet = builder.execute();
 		
 		InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<T> instancesBuilder = new InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<T>();
 		InstanceHelper.Builder.OneDimensionArray.Adapter.Default<T> instanceBuilder = new InstanceHelper.Builder.OneDimensionArray.Adapter.Default<T>(aClass){
@@ -112,202 +116,21 @@ public class IesaExcelToDatabase extends AbstractIesaBusinessIT {
 			@Override
 			protected T __execute__() {
 				T identifiable = super.__execute__();
-				setGlobalIdentifier(identifiable);
+				if(identifiable.getIdentifier()==null)
+					setGlobalIdentifier(identifiable);
 				return identifiable;
 			}
 		};
 		instanceBuilder.setKeyBuilder(new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
 			private static final long serialVersionUID = 1L;
 			protected ArrayHelper.Dimension.Key __execute__() {
-				return new ArrayHelper.Dimension.Key((String)getInput()[0]);
+    			return new ArrayHelper.Dimension.Key(getGlobalIdentifier((String)getInput()[0]).getCode());
 			}
 		}).addParameterArrayElementString(fields);
 		instancesBuilder.setOneDimensionArray(instanceBuilder);
 		
 		inject(BusinessInterfaceLocator.class).injectTyped(aClass).synchronize(sheet,instanceBuilder);
     }
-    
-    private void createJobTitle(){
-    	InstanceHelper.Pool.getInstance().load(JobTitle.class);
-    	MicrosoftExcelHelper.Workbook.Sheet.Builder builder = new MicrosoftExcelHelper.Workbook.Sheet.Builder.Adapter.Default(workbookFileName,JobTitle.class);    	
-    	builder.createMatrix().getMatrix().getRow().setFromIndex(1);
-    	
-    	builder.getMatrix().getRow().setKeyBuilder(new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
-			private static final long serialVersionUID = 1L;
-    		@Override
-    		protected Key __execute__() {
-    			GlobalIdentifier globalIdentifier = getGlobalIdentifier((String)getInput()[0]);
-    			return new ArrayHelper.Dimension.Key(globalIdentifier.getCode());
-    		}
-    	});
-    	builder.getMatrix().getRow().getKeyBuilder().addParameters(new Object[]{0});
-    	builder.getMatrix().getRow().addIgnoredKeyValues(InstanceHelper.getInstance().callGetMethod(InstanceHelper.Pool.getInstance().get(JobTitle.class), String.class, GlobalIdentifier.FIELD_CODE));
-    	
-    	//builder.getMatrix().getRow().createKeyBuilder(new Object[]{0}, InstanceHelper.getInstance()
-    	//		.callGetMethod(InstanceHelper.Pool.getInstance().get(JobTitle.class), String.class, GlobalIdentifier.FIELD_CODE));
-		
-    	MicrosoftExcelHelper.Workbook.Sheet sheet = builder.execute();
-		
-		InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<JobTitle> instancesBuilder = new InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<JobTitle>();
-		InstanceHelper.Builder.OneDimensionArray.Adapter.Default<JobTitle> instanceBuilder = new InstanceHelper.Builder.OneDimensionArray.Adapter.Default<JobTitle>(JobTitle.class){
-			private static final long serialVersionUID = 1L;
-			@Override
-			protected JobTitle __execute__() {
-				JobTitle jobTitle = super.__execute__();
-				if(jobTitle.getIdentifier()==null)
-					setGlobalIdentifier(jobTitle);
-				else{
-					//System.out.println(jobTitle.getIdentifier()+" : "+jobTitle.getCode()+" : "+jobTitle.getGlobalIdentifier().getIdentifier());
-				}
-				return jobTitle;
-			}
-		};
-		instanceBuilder.setKeyBuilder(new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
-			private static final long serialVersionUID = 1L;
-			protected ArrayHelper.Dimension.Key __execute__() {
-				GlobalIdentifier globalIdentifier = getGlobalIdentifier((String)getInput()[0]);
-    			return globalIdentifier == null ? null : new ArrayHelper.Dimension.Key(globalIdentifier.getCode());
-			}
-		}).addParameterArrayElementString(FieldHelper.getInstance().buildPath(AbstractIdentifiable.FIELD_GLOBAL_IDENTIFIER,GlobalIdentifier.FIELD_IDENTIFIER));
-		instancesBuilder.setOneDimensionArray(instanceBuilder);
-		
-		inject(JobTitleBusiness.class).synchronize(sheet,instanceBuilder);
-    }
-    
-    private void createPersons(){
-    	InstanceHelper.Pool.getInstance().load(Person.class);
-    	MicrosoftExcelHelper.Workbook.Sheet.Builder builder = new MicrosoftExcelHelper.Workbook.Sheet.Builder.Adapter.Default(workbookFileName,Person.class);    	
-    	builder.createMatrix().getMatrix().getRow().setFromIndex(1).createKeyBuilder(new Object[]{0}, InstanceHelper.getInstance()
-    			.callGetMethod(InstanceHelper.Pool.getInstance().get(Person.class), String.class, GlobalIdentifier.FIELD_CODE));
-		MicrosoftExcelHelper.Workbook.Sheet sheet = builder.execute();
-		
-		InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<Person> instancesBuilder = new InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<Person>();
-		InstanceHelper.Builder.OneDimensionArray.Adapter.Default<Person> instanceBuilder = new InstanceHelper.Builder.OneDimensionArray.Adapter.Default<Person>(Person.class){
-			private static final long serialVersionUID = 1L;
-			@Override
-			protected Person __execute__() {
-				Person person = super.__execute__();
-				setGlobalIdentifier(person);
-				return person;
-			}
-		};
-		instanceBuilder.setKeyBuilder(new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
-			private static final long serialVersionUID = 1L;
-			protected ArrayHelper.Dimension.Key __execute__() {
-				return new ArrayHelper.Dimension.Key((String)getInput()[0]);
-			}
-		}).addParameterArrayElementStringIndexInstance(0,FieldHelper.getInstance().buildPath(AbstractIdentifiable.FIELD_GLOBAL_IDENTIFIER,GlobalIdentifier.FIELD_IDENTIFIER)
-				,1,Person.FIELD_LASTNAMES);
-		instancesBuilder.setOneDimensionArray(instanceBuilder);
-		
-		inject(PersonBusiness.class).synchronize(sheet,instanceBuilder);
-    }
-    
-    public void persons(){
-    	MicrosoftExcelHelper.Workbook.Sheet.Builder builder = new MicrosoftExcelHelper.Workbook.Sheet.Builder.Adapter
-    			.Default(System.getProperty("user.dir")+"\\src\\test\\resources\\data\\iesa\\2016_2017_Trimester_1.xlsx",0);
-    	
-    	//builder.createMatrix().getMatrix().getRow().createKeyBuilder(new Object[]{0}, new String[]{"TS/OF/061/2016"});
-    	Collection<String> codes = new ArrayList<>();
-		for(Person person : inject(PersonDao.class).readAll())
-			codes.add(person.getCode());
-    	builder.createMatrix().getMatrix().getRow().createKeyBuilder(new Object[]{0}, codes.toArray(new String[]{}));
-		MicrosoftExcelHelper.Workbook.Sheet sheet = builder.execute();
-		
-		InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<Person> instanceBuilder = new InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<Person>(null);
-		
-		InstanceHelper.Builder.OneDimensionArray.Adapter.Default<Person> oneDimensionArray = new InstanceHelper.Builder.OneDimensionArray.Adapter.Default<Person>(Person.class);
-		oneDimensionArray.setKeyBuilder(
-				new ArrayHelper.Dimension.Key.Builder.Adapter.Default(){
-					private static final long serialVersionUID = 1L;
-
-					protected ArrayHelper.Dimension.Key __execute__() {
-						return new ArrayHelper.Dimension.Key((String)getInput()[0]);
-					}
-				} ).addParameterArrayElementString("globalIdentifier.code");
-		instanceBuilder.setOneDimensionArray(oneDimensionArray);
-		
-		assertEquals(0l, inject(PersonDao.class).countAll());
-		
-		inject(PersonBusiness.class).synchronize(sheet,new InstanceHelper.Builder.OneDimensionArray.Adapter.Default<Person>(Person.class)
-				.addParameterArrayElementString("globalIdentifier.code"));
-		
-		assertEquals(836l, inject(PersonDao.class).countAll());
-		
-		/**/
-		builder = new MicrosoftExcelHelper.Workbook.Sheet.Builder.Adapter
-    			.Default(System.getProperty("user.dir")+"\\src\\test\\resources\\data\\iesa\\2016_2017_Trimester_1.xlsx",0);
-    	
-		codes = new ArrayList<>();
-		for(Person person : inject(PersonDao.class).readAll())
-			codes.add(person.getCode());
-    	builder.createMatrix().getMatrix().getRow().createKeyBuilder(new Object[]{0}, codes.toArray(new String[]{}));
-    	
-    	builder.getMatrix().getRow();
-		sheet = builder.execute();
-		
-		instanceBuilder = new InstanceHelper.Builder.TwoDimensionArray.Adapter.Default<Person>(null);
-		instanceBuilder.setOneDimensionArray(oneDimensionArray);
-		
-		assertEquals(836l, inject(PersonDao.class).countAll());
-		
-		inject(PersonBusiness.class).synchronize(sheet,oneDimensionArray);
-		
-		assertEquals(836l, inject(PersonDao.class).countAll());
-		
-		/*
-		Collection<Person> createPersons = instanceBuilder.setInput(sheet.getValues()).execute();
-		Collection<Person> updatePersons = instanceBuilder.setInput(sheet.getIgnoreds()).execute();
-		
-		System.out.println(createPersons);
-		assertEquals(835, createPersons.size());
-		
-		System.out.println(updatePersons);
-		assertEquals(1, updatePersons.size());
-		*/
-		/*
-		IdentifiableExcelSheetReader<Person> excelSheetReader = new IdentifiableExcelSheetReader<Person>(file,Person.class);
-		OneDimensionObjectArrayAdapter<Person> setter = new OneDimensionObjectArrayAdapter<Person>(Person.class);
-		
-    	org.cyk.utility.common.accessor.InstanceFieldSetter.TwoDimensionObjectArray.Adapter.Default<Person> twoDimensionObjectArray = new org.cyk.utility.common.accessor.InstanceFieldSetter.TwoDimensionObjectArray.Adapter.Default<Person>(setter){
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public Boolean getIgnoreExistingKey(Object[] values, Object key,Object keyType) {
-				return Boolean.TRUE;
-			}
-			
-			@Override
-			public Person getInstanceByKey(Object[] values, Object key, Object type) {
-				Person person = getPerson(values);
-				if(values[0].equals("IESA/2014MUJ0441-KG"))
-					System.out.println("C : "+values[3]+" : "+person.getBirthDate());
-				try {
-					String[] p = StringUtils.split((String)values[3], Constant.CHARACTER_SPACE.toString());
-					if(p!=null && p.length>0 && StringUtils.isNotBlank(p[0])){
-						person.setBirthDate(DateUtils.parseDate(p[0], "MM/dd/yyyy"));
-						if(person.getCode().equals("IESA/2014MUJ0441-KG"))
-							System.out.println(person.getBirthDate());
-						personsUpdated.add(person);
-					}
-				} catch (ParseException e) {
-					System.out.println("Cannot parse : "+values[0]+" : <<"+values[3]+">>");
-				}
-				return person;
-			}
-		};
-		
-		excelSheetReader.execute();
-		twoDimensionObjectArray.setInput(excelSheetReader.getValues());
-		twoDimensionObjectArray.execute();
-		
-		System.out.print("Updating persons : "+personsUpdated.size()+"...");
-		long t = System.currentTimeMillis();
-		update(personsUpdated);
-		System.out.println( ((System.currentTimeMillis()-t)/1000) );
-		*/
-    }
-    
     
     @Override
     protected AbstractFakedDataProducer getFakedDataProducer() {
